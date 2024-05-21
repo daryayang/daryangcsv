99149b791e4d6102be18087644913fc1d896a497
daryayang <840582086@qq.com>
2024-05-21 22:10:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# Explore Your Data!
+
+**You can analyze your data using natural language **
+
+PandasAI makes dataframes conversational
+
+Langchain is a framework interacts between data and language models
+
+Streamlit is a framework builds interactive apps with data
+
+**Installation**
+
+Create a virtual environment and install packages for an effective package management
+
+```
+pip install streamlit
+pip install pandasai
+pip install openpyxl
+pip install sentence_transformer
+pip install rapidfuzz
+```
+
+**Running**
+
+Run listed commands in the terminal respectively
+
+
+```
+source your_environment_name/bin/activate
+streamlit run \your_python_file_name.py
+```
+
+***Load Your Data***
+
+<img width="1429" alt="2" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/164cd78b-dad1-40ee-9b74-5f07618e220a">
+
+
+***Get Statistics***
+<img width="1420" alt="7" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/c0962be5-1221-4e23-90ef-1eb1af59b2f4">
+
+<img width="1439" alt="6" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/ace92cac-7cfc-4e42-8b11-6c6b88440de1">
+
+
+***Ask Question & Generate Response***
+<img width="1440" alt="3" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/70f8665b-cf0f-4b32-aab3-5eb8172f7d0a">
+
+
+***Plot Graph***
+<img width="1440" alt="4" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/6cc379be-9d8b-40f8-afb9-101c955dd8c7">
+
+
+***Get Generated Code***
+<img width="1440" alt="5" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/68dbb9b0-cfc3-4aad-8305-68e85f799dd1">
+
+
+***Get General Information for Columns according to Similarity Score***
+
+
+<img width="1437" alt="2" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/3282411d-6d3a-4d8f-96eb-f96438428720">
+
+<img width="1437" alt="3" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/96601bff-6711-4150-aae4-4b30a906469a">
+
+
+<img width="1427" alt="4" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/7513d6b9-cb29-4da7-9501-8b2367dcdc7b">
+
+<img width="1431" alt="5" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/f233c1fa-9687-47ac-8237-4dcc21fa5a23">
+
+
+<img width="1439" alt="6" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/9bda260d-4a82-4256-b64f-169ba7af9145">
+
+
+<img width="1434" alt="7" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/9f003fe3-fec7-4aed-9a2b-f408ac8f6904">
+
+
+<img width="1429" alt="8" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/ffafcc3d-d96d-43cc-ab3d-0989185f85a1">
+
+<img width="1440" alt="9" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/99f98c9f-55a5-44f8-929d-2bd8ac9f9fbd">
+
+
+***Get An Explanation with Agent***
+
+<img width="1419" alt="6" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/386b721b-6519-49fa-ba1c-dfd3131fb2cd">
+
+
+***Get Clarification Questions with Agent***
+
+<img width="1413" alt="8" src="https://github.com/fatma010/langchain_pandasai_streamlit/assets/73019838/6c93ecbd-3472-4321-bcfa-81cc3c2d0b9c">
+
+
+
+
+
+
+
+
---
+++ pandasai_agent.py
@@ -0,0 +1,221 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+import pandas as pd
+from pandasai.llm.openai import OpenAI
+from pandasai.responses.streamlit_response import StreamlitResponse
+import matplotlib.pyplot as plt
+import plotly.express as px
+from langchain.evaluation import load_evaluator
+from pandasai import Agent
+from pandasai.skills import skill
+
+#set plot option for streamlit
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+#set maximum row,column size for uploaded files
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+pd.set_option("display.max_colwidth", None)
+
+# load environment variables
+load_dotenv()
+
+# check open_ai_api_key in env, if it is not defined as a variable
+#it can be added manually in the code below
+
+if os.environ.get("OPEN_AI_API_KEY") is None or os.environ.get("OPEN_AI_API_KEY") =="":
+    print("open_ai_api_key 未设置为环境变量")
+else:
+    print("Open AI API 密钥已设置")
+
+#get open_ai_api_key
+OPEN_AI_API_KEY= os.environ.get("OPEN_AI_API_KEY")
+
+# set tittle for Streamlit UI
+st.title("数据智能分析")
+st.info("上传文件并开始提问")
+
+#set formats of files allowed to upload
+file_formats = {
+    "csv": pd.read_csv,
+    "xls": pd.read_excel,
+    "xlsx": pd.read_excel,
+    "xlsm": pd.read_excel,
+    "xlsb": pd.read_excel,
+}
+
+#define a function for file formats
+#check the file format among the list above
+def load_data(uploaded_file):
+   # ext = os.path.splitext(uploaded_file.name)[1][1:].lower()
+    try:
+        ext = os.path.splitext(uploaded_file.name)[1][1:].lower()
+    except:
+        ext = uploaded_file.split(".")[-1]
+    if ext in file_formats:
+        return file_formats[ext](uploaded_file)
+    #if file format does not match give message
+    else:
+        st.error(f"不支持的文件格式: {ext}")
+        return None
+
+#cumulative sum for the column
+def cum_sum(dataframe,col):
+    return dataframe(col).cumsum()
+
+
+#upload a file from ui
+uploaded_files = st.file_uploader("请上传数据文件",
+                                 type =list(file_formats.keys()),)# accept_multiple_files=True,
+
+#check the uploaded file whether empty or not
+if uploaded_files:
+    dataframe = load_data(uploaded_files)
+    if dataframe.empty:
+        #if file is empty give a message
+        st.write("给定的数据为空，请上传完整文件以提问")
+    # if file is full, list first 3 rows
+    else:
+        st.write(dataframe.head(3))
+    # give a general description of data
+    if st.sidebar.button("统计"):
+        if dataframe.empty:
+            st.write("空文件无法进行描述")
+    # if uploaded file is full, return description of data
+        else:
+            df_desc = dataframe.describe()
+            st.write(df_desc)
+    if st.sidebar.button("信息"):
+        df_info=dataframe.info()
+        st.write(df_info)
+    if st.sidebar.button("平均值"):
+        df_mean=dataframe.mean()
+        st.write(df_mean)
+    if st.sidebar.button("中位数"):
+        df_med=dataframe.median()
+        st.write(df_med)
+    if st.sidebar.button("样本"):
+        df_sample=dataframe.sample()
+        st.write(df_sample)
+    if st.sidebar.button("相关性"):
+        df_corr = dataframe.corr()
+        st.write(df_corr)
+    if st.sidebar.button("删除所有重复项"):
+        df_drop_all=dataframe.drop_duplicates()
+        st.write(df_drop_all)
+    if st.sidebar.button("删除所有空值"):
+        df_drop_null =dataframe.dropna(how='all')
+        st.write(df_drop_null)
+
+# define preffered llm
+    llm = OpenAI(api_token='OPEN_AI_API_KEY',model="gpt-3.5-turbo-1106")
+
+# create agent
+
+    agent = Agent([dataframe],
+              config={"save_logs": True,
+                                      "verbose": True,
+                                      "enforce_privacy": True,
+                                      "enable_cache": True,
+                                      "use_error_correction_framework": True,
+                                      "max_retries": 3,
+                                      #"custom_prompts": {},
+                                      "open_charts": True,
+                                      "save_charts": False,
+                                      "save_charts_path": "exports/charts",
+                                      "custom_whitelisted_dependencies": [],
+                                      "llm": llm,
+                                      #"llm_options": null,
+                                      "saved_dfs": [],
+                                     "response_parser": StreamlitResponse
+                                      #  share a custom sample head to the LLM  "custom_head": head_df
+
+                                 }
+                                    , memory_size=10
+                              )
+
+    #adding agent skilss
+
+    agent.add_skills(cum_sum)
+
+
+    # get question from user
+    prompt =st.text_area("请输入您的问题:")
+    st.info("输入您的问题，如果要获取图表，可以指定图表类型")
+
+
+
+#get the request to generate an asnwer for the question
+# check uploaded file, if the file is empty give a message
+# show the model's response as an answer
+
+    if st.button("生成回复"):
+        if dataframe.empty:
+            st.write("空文件无法生成回复")
+        else:
+            if prompt:
+                st.spinner("计算中...")
+            gen_code = agent.last_code_executed
+            #gen_response = str(agent.chat(prompt))
+
+            tab1, tab2, tab3, tab4, tab5  = st.tabs(["Generated Response",
+                                               "Generated Chart",
+                                               "Explanation",
+                                               "Generated Code",
+                                               "Clarification Questions"]
+                                             )
+            with tab1:
+                    if prompt:
+                        st.write(str(agent.chat(prompt)))
+
+            with tab2:
+                if 'pie' in prompt.lower():
+                    fig = px.pie({prompt})
+                    st.pyplot(plt.gcf())
+                if 'bar' in prompt.lower():
+                    fig = px.bar({prompt})
+                    st.pyplot(plt.gcf())
+                if 'bubble' in prompt.lower():
+                    fig = px.scatter({prompt})
+                    st.pyplot(plt.gcf())
+                if 'dot' in prompt.lower():
+                    fig = px.scatter({prompt})
+                    st.pyplot(plt.gcf())
+                if 'time series' in prompt.lower():
+                    fig = px.line({prompt})
+                    st.pyplot(plt.gcf())
+                else:
+                    fig = px.histogram({prompt})
+                    st.pyplot(plt.gcf())
+
+
+            with tab3:
+                st.write(agent.explain())
+
+
+            with tab4:
+                st.write(f" {prompt}的生成代码 :")
+                st.write(agent.last_code_executed)
+
+            with ((((tab5)))):
+                clarification_question = str(agent.clarification_questions(agent.chat(prompt)))
+                clarification_question = clarification_question.replace("[","")
+                clarification_question = clarification_question.replace("]","")
+                clarification_question = clarification_question.replace(",","\n")
+                clarification_question = clarification_question.replace("'","\n")
+
+                st.write(str(clarification_question))
+
+
+
+                    # if the question is empty give a message
+    else:
+        st.warning("请提问")
+
+
+
+
+
+
